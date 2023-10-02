d18625337eb00bafd4bc24491fa2dd6c35e1cda4
Dimch93 <dimch93@ukr.net>
2023-10-02 16:53:34 +0300

    home-10.2

--- src/index.js
+++ src/index.js
@@ -1,50 +1,45 @@
-// live_l1N2KaqFBnWSPpFL0pMbcd4AalFUPTVvgIzIzgCup7Re6x2pR2M90j5F1sKu3feC;
-
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 
-const url = `https://api.thecatapi.com/v1/breeds`;
-const api_key =
-  'live_l1N2KaqFBnWSPpFL0pMbcd4AalFUPTVvgIzIzgCup7Re6x2pR2M90j5F1sKu3feC';
-
-const refs = {
-  breedSelect: document.querySelector('.breed-select'),
+const ref = {
+  breeSelect: document.querySelector('.breed-select'),
+  catInfo: document.querySelector('.cat-info'),
   loader: document.querySelector('.loader'),
   error: document.querySelector('.error'),
-  catInfo: document.querySelector('.cat-info'),
 };
+const { breeSelect, catInfo, loader, error } = ref;
 
 loader.classList.replace('loader', 'is-hidden');
 error.classList.add('is-hidden');
 catInfo.classList.add('is-hidden');
 
-let storedBreeds = [];
+let arrBreedsId = [];
 fetchBreeds()
   .then(data => {
     data.forEach(element => {
-      storedBreeds.push({ text: element.name, value: element.id });
+      arrBreedsId.push({ text: element.name, value: element.id });
     });
     new SlimSelect({
-      select: selector,
-      data: storedBreeds,
+      select: breeSelect,
+      data: arrBreedsId,
     });
   })
   .catch(onFetchError);
 
-breedSelect.addEventListener('change', onBreedSelect);
+breeSelect.addEventListener('change', onBreedSelect);
 
 function onBreedSelect(event) {
-  breedSelect.classList.replace('is-hidden', 'loader');
-  selector.classList.add('is-hidden');
+  loader.classList.replace('is-hidden', 'loader');
+  breeSelect.classList.add('is-hidden');
   catInfo.classList.add('is-hidden');
 
   const breedId = event.currentTarget.value;
   fetchCatByBreed(breedId)
     .then(data => {
       loader.classList.replace('loader', 'is-hidden');
-      breedSelect.classList.remove('is-hidden');
+      breeSelect.classList.remove('is-hidden');
       const { url, breeds } = data[0];
 
       catInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
@@ -54,7 +49,7 @@ function onBreedSelect(event) {
 }
 
 function onFetchError(error) {
-  breedSelect.classList.remove('is-hidden');
+  breeSelect.classList.remove('is-hidden');
   loader.classList.replace('loader', 'is-hidden');
 
   Notify.failure(
